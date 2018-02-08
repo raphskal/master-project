f22e29c4ac520f78e8528b835507048b64e437eb
raphskal <raphael.kalender@gmail.com>
2018-02-08 09:36:45 +0100

    add

--- custom_source.py
+++ custom_source.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+This prorgram creates a hard-coded template based on fits to the 16geu data
+with SNooPy. It can be used in call_MIfit.py for the fitting process.
+
+
 Created on Mon Nov  6 09:44:46 2017
 
 @author: kalender
@@ -9,6 +13,7 @@ from scipy.interpolate import RectBivariateSpline
 from scipy.interpolate import interp1d
 import sncosmo
 import os
+import matplotlib.pyplot as plt
 from astropy.io import ascii
 
 
@@ -17,25 +22,28 @@ class sn16geu(sncosmo.Source):
     _param_names = ['amplitude','s','t0']
     param_names_latex = ['A','stretch','t_0']   # used in plotting display
 
-    def __init__(self,t0, name='sn16geu', version=1.0):
+    def __init__(self,t0,amplitude, name='sn16geu', version=1.0):
         self.name = name
         self.version = version
         self.fixt0 = t0
-        self._parameters = np.array([1.,1.,t0])
+        self._parameters = np.array([amplitude,1.,t0])
         
-        self.bands = ['f110w','f160w','f625w','f814w','P60g','P60i','P60r','P48R']
+        self.bands = ['f110w','f160w','uvf625w','uvf814w','P60g','P60i','P60r','P48R']
         
         self._model_flux = {}
         
         for name in self.bands:
-            c = ascii.read('custom/iPTF16geu_lc_'+name+'_model.dat')
+            c = ascii.read('custom/model/iPTF16geu_lc_'+name+'_model.dat')
             t = c['time']
             flux = 10**(-0.4*c['magnitude'])
             interp = interp1d(t,flux)
             tgrid = np.linspace(t[0],t[-1],num=100)
-            max_time = tgrid[np.argmax(interp(tgrid))]
-            tgrid = tgrid - max_time
-            self._model_flux[name] = interp1d(tgrid,interp(tgrid+max_time))
+            flux = interp(tgrid)
+            #max_time = tgrid[np.argmax(interp(tgrid))]
+            plt.plot(tgrid,flux)
+            plt.savefig('custom/plots/'+name+'_lc.png')
+            plt.close()
+            self._model_flux[name] = [tgrid,flux]
             
         print 'Custom source for 16geu was initiated'
             
@@ -46,13 +54,22 @@ class sn16geu(sncosmo.Source):
     
     def bandflux(self, wave, phase,zp,zpsys):
         A,s,t0 = self._parameters
-        fluxes = np.empty((len(wave)))
-        for i in range(len(fluxes)):
-            try:
-                fluxes[i] = A*self._model_flux[wave[i].name]((phase[i]-t0)/s)
-            except ValueError:
-                raise ValueError('Phase is not in sn16geu-model')
-            
+        fluxes = np.empty((len(wave))) 
+        if type(wave) is np.string_:
+                interp = interp1d(self._model_flux[wave][0]-t0,self._model_flux[wave][1])
+                fluxes = A*interp((phase-t0)/s)
+        else:
+            for i in range(len(fluxes)):
+                if type(wave[i]) is str:
+                    interp = interp1d(self._model_flux[wave[i]][0]-t0,self._model_flux[wave[i]][1])
+                    fluxes = A*interp((phase[i]-t0)/s)
+                else:
+                    try:
+                        interp = interp1d(self._model_flux[wave[i].name][0]-t0,self._model_flux[wave[i].name][1])
+                        fluxes[i] = A*interp((phase[i]-t0)/s)
+                    except ValueError:
+                        raise ValueError('Phase = '+str(phase[i]-t0)+' for wave = '+str(wave[i].name)+' is not in sn16geu-model')
+                
         return fluxes
                 
 
