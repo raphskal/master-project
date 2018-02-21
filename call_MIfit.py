ea7269b71b147cda46787037785011816209a727
raphskal <raphael.kalender@gmail.com>
2018-02-21 15:32:33 +0100

    add

--- call_MIfit.py
+++ call_MIfit.py
@@ -17,6 +17,8 @@ from custom_source import sn16geu
 from astropy.table import vstack
 from astropy.io import ascii
 from MI_nestlefit import MI_model, nest_lc
+from scipy.interpolate import interp1d
+from stats import histogram,samples
 import sncosmo
 import os
 import corner
@@ -129,7 +131,7 @@ mH.set(mwr_v=3.1)
 mH.set(z=z,lensz=lensz)
 mH.set(t0=it0)
 # Set the initial model, construct a MI_model with it
-mH.set(amplitude=3.e-8,hostebv=0.1,hostr_v=1.4,lensr_v=5.0)
+mH.set(amplitude=3.e-8,hostebv=0.1,hostr_v=1.4,lensr_v=3.1)
 
 
 # Setup model based on SNoopy fit for 16geu 
@@ -140,7 +142,7 @@ if __name__ == '__main__':
     # The parameters that are varied in the fit
     fit_param = ['s','hostebv','hostr_v',
     't01','t02','t03','t04',
-    'lensr_v','lensr_v1','f',
+    'f',
     'lensebv2','lensebv3','lensebv4',
     'amplitude1','amplitude2','amplitude3','amplitude4']
     
@@ -159,55 +161,31 @@ if __name__ == '__main__':
                   #'amplitude3': (1.e+7,1.e+10), 'amplitude4': (1.e+6,1.e+9)}
                   'amplitude1': (1.e-9,1.e-7), 'amplitude2': (1.e-9,1.e-7),
                   'amplitude3': (1.e-9,1.e-7), 'amplitude4': (1.e-10,1.e-8),
-                  'lensr_v':(3.0,6.0), 'f':(1.4,2.0), 'lensr_v1':(1.0,3.0)} 
+                  'lensr_v':(1.0,4.0), 'f':(1.4,2.0), 'lensr_v1':(1.0,3.0)} 
  
     # Start the nestlefit with the hsiao-stretch or the custom_model
     # Save the parameters in 'nestfitparam.dat' and plot the model
 
-    MI_mH = MI_model(mH,4,f=1.7,useprior=True, samerv=False)
+    MI_mH = MI_model(mH,4,f=1.7,useprior=True, samerv=True)
     model, res = nest_lc(phot_d,MI_mH,fit_param,fit_bounds)
     #MI_geu_source = MI_model(geu_source,4)
     #model,res = nest_lc(phot_d,MI_geu_source,fit_param_16geu,fit_bounds)
     
-    samples, params  =  res.samples, res.vparam_names
-    u,v = samples[:,params.index('hostebv')],samples[:,params.index('hostr_v')]
-    w,x = samples[:,params.index('lensebv2')],samples[:,params.index('lensr_v')]
-    y,a = samples[:,params.index('f')],samples[:,params.index('lensebv3')]
-    b = samples[:,params.index('lensebv4')]
-    z = np.empty((len(x),7))
-    for i in range(len(x)):
-        z[i] = np.array([u[i],v[i],w[i],x[i],y[i],a[i],b[i]])
-    corner.corner(z, smooth=1, labels=['hostebv','hostr_v','lensebv2','lensr_v','f','lensebv3','lensebv4'])
-    plt.savefig('samples.png')
-    plt.close()
+    # Draw the correlations between the parameters that are degenerated
+    samp, params  =  res.samples, res.vparam_names
+    samples(samp,params,['hostebv','hostr_v','lensebv2','f','lensebv3','lensebv4'])
 
-        
+    # Draw the probability density function
+    df,data = res.ndof, model.tot_amp
+    fmin,minamp = min(model.chi), data[np.argmin(model.chi)]
+    histogram(data)
     
-    df = res.ndof
-    fmin = min(model.chi)
-    x = np.linspace(chi2.ppf(0.01, df), chi2.ppf(0.99, df), 100)
-    pdf = chi2.pdf(x, df)
-    prob = quad(chi2.pdf,fmin,np.Infinity,args=(df,))[0]
-    plt.plot(x, pdf)
-    plt.fill_between(x,0.,pdf, where=x>=fmin,alpha=0.5)
-    plt.text(fmin,pdf[5],str(prob))
-    plt.title('$chi^2$ pdf, df='+str(df))
-    plt.plot()
-    plt.savefig('chi2_pdf.png')
-    plt.close()
-    plt.plot(model.tot_amp,model.chi,'x')
-    plt.ylim(0,1.e+3)
-    plt.title('$chi^2$ with respect to total amplification')
-    plt.xlabel('total amplification')
-    plt.ylabel('$chi^2$')
-    plt.savefig('chi_totamp.png')
-    plt.close()
-    
+    # Plot the lightcurves for the model and write the parameters into a file
     model.plot(phot_d)
     myfile = open('nestfitparam.dat','w')
     for name in fit_param:
         myfile.write(name+' = '+str(model.get(name))+' ('+str(res.errors[name])+')\n')
     myfile.write("chiqsquare = "+str(fmin)+'\n')
-    myfile.write('total amplification = '+str(model.tot_amp[np.argmin(model.chi)]))
+    myfile.write('total amplification = '+str(minamp))
     myfile.close()
     
